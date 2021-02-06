03ae96bc66b355ca43b75185ab606905e9991f56
r-aravind <aravind.ar786@gmail.com>
2021-02-06 21:17:12 +0530

    update: - added class-based view Meme - added get and post methods - updated urlpatterns - added csrf exemption

--- memes/views.py
+++ memes/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+
+import json
 
 
-# Create your views here.
 class Meme(View):
 
-    def get(self, request, userid):
-        pass
+    @method_decorator(csrf_exempt)
+    def dispatch(self, *args, **kwargs):
+        return super(Meme, self).dispatch(*args, **kwargs)
+
+    def get(self, request, userid=None):
+        if userid is None:
+            return HttpResponse("Hello yeet")
+
+        return HttpResponse(
+            json.dumps({
+                "test": userid
+            }), content_type="application/json"
+        )
+
+    def post(self, request):
+        post_data = request.POST
+        return HttpResponse(
+            json.dumps({
+                "name": post_data["name"],
+                "caption": post_data["caption"],
+                "url": post_data["url"]
+            }), content_type="application/json"
+        )
--- xmeme_api/urls.py
+++ xmeme_api/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from memes.views import Meme
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/meme/<str:userid>', Meme.as_view()),
+    path('api/meme/', Meme.as_view())
 ]
