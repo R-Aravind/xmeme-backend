85e6a1e101046eb00b4f9d81fb492e4a852b2947
r-aravind <aravind.ar786@gmail.com>
2021-02-06 22:18:51 +0530

    update: - added model classes Meme and Comment - added fields - modified save method to set timezone automatically

--- memes/models.py
+++ memes/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Meme(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    url = models.URLField()
+    caption = models.CharField(max_length=400)
+
+    likes = models.IntegerField()
+    created = models.DateTimeField()
+    modified = models.DateTimeField()
+
+    def save(self, *args, **kwargs):
+        """ On save, update timestamps """
+        if not self.id:
+            self.created = timezone.now()
+        self.modified = timezone.now()
+        return super(Meme, self).save(*args, **kwargs)
+
+
+class Comment(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    meme = models.ForeignKey(Meme, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    content = models.CharField(max_length=400)
+
+    def save(self, *args, **kwargs):
+        """ On save, update timestamps """
+        if not self.id:
+            self.created = timezone.now()
+        return super(Comment, self).save(*args, **kwargs)
