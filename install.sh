2a56dee1025df88a36584032026509c042fffdc7
R Aravind <aravind.ar786@gmail.com>
2021-02-12 00:40:06 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,11 @@
 
 
 # Any installation related commands
+sudo apt-get update
 
-sudo apt-get install -y python3 python3-pip
+sudo apt-get install -y python3 
+sudo apt-get install -y python3-pip
 
-pip install -r ./requirements.txt
+pip3 install -r ./requirements.txt
 
 # Any configuration related commands
